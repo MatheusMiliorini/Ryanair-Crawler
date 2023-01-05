4e282c9e764e0a4d3f74e11ba1b3fbbaeeadceaf
MatheusMiliorini <miliorinimatheus@gmail.com>
2023-01-05 18:26:07 +0000

    New parameters as variables

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ dotenv.config();
 import axios from "axios";
 import { MongoClient } from "mongodb";
 
-const DATE_START = "2023-01-07";
+const DATE_START = "2023-01-06";
 const DEPARTURE_AIRPORT = "DUB"; // Shannon - SNN / Dublin - DUB
-const DAYS = 2;
+const DAYS_MIN = 3;
+const DAYS_MAX = 3; // Apparently, this does nothing :)
 const ITERATIONS = 50;
 const WEEK = 7;
+const OUTBOUND_TIME_START = "00:00";
 const OUTBOUND_TIME_LIMIT = "12:00";
-const RETURN_TIME_LIMIT = "12:00";
+const RETURN_TIME_START = "12:00";
+const RETURN_TIME_LIMIT = "23:59";
 
 const client = new MongoClient(process.env.MONGO_URI);
+const promises = [];
 
 async function run() {
+  const runId = Math.floor(Math.random() * 100000000);
+  console.log("runId: ", runId);
+
   const flights = client
     .db(process.env.MONGO_DB)
     .collection(process.env.MONGO_COLLECTION);
@@ -22,18 +29,19 @@ async function run() {
     let startDate = new Date(DATE_START);
     startDate.setDate(startDate.getDate() + WEEK * i);
     let endDate = new Date(
-      new Date(startDate.getTime()).setDate(startDate.getDate() + DAYS)
+      new Date(startDate.getTime()).setDate(startDate.getDate() + DAYS_MIN)
     );
     startDate = startDate.toISOString().split("T")[0];
     endDate = endDate.toISOString().split("T")[0];
 
     const { data } = await axios.get(
-      `https://www.ryanair.com/api/farfnd/v4/roundTripFares?departureAirportIataCode=${DEPARTURE_AIRPORT}&outboundDepartureDateFrom=${startDate}&market=en-ie&adultPaxCount=1&outboundDepartureDateTo=${startDate}&inboundDepartureDateFrom=${endDate}&inboundDepartureDateTo=${endDate}&outboundDepartureDaysOfWeek=SATURDAY&durationFrom=${DAYS}&durationTo=${DAYS}&outboundDepartureTimeFrom=00:00&outboundDepartureTimeTo=${OUTBOUND_TIME_LIMIT}&inboundDepartureTimeFrom=00:00&inboundDepartureTimeTo=${RETURN_TIME_LIMIT}`
+      `https://www.ryanair.com/api/farfnd/v4/roundTripFares?departureAirportIataCode=${DEPARTURE_AIRPORT}&outboundDepartureDateFrom=${startDate}&market=en-ie&adultPaxCount=1&outboundDepartureDateTo=${startDate}&inboundDepartureDateFrom=${endDate}&inboundDepartureDateTo=${endDate}&durationFrom=${DAYS_MIN}&durationTo=${DAYS_MAX}&outboundDepartureTimeFrom=${OUTBOUND_TIME_START}&outboundDepartureTimeTo=${OUTBOUND_TIME_LIMIT}&inboundDepartureTimeFrom=${RETURN_TIME_START}&inboundDepartureTimeTo=${RETURN_TIME_LIMIT}`
     );
     const { fares } = data;
     console.log(`[${i}] Inserting ${fares.length} flights into DB...`);
     fares.forEach((fare) => {
       const data = {
+        runId,
         outbound: {
           departure: fare.outbound.departureDate,
           arrival: fare.outbound.arrivalDate,
@@ -60,7 +68,7 @@ async function run() {
   await Promise.all(promises);
   console.log("Finished all promises");
   await client.close();
-  console.log('MongoDB connection closed.');
+  console.log("MongoDB connection closed.");
   process.exit();
 }
 
